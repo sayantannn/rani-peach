da1258f7486274204883641c96d8bc4aca7104ef
sayantannn <124330682+sayantannn@users.noreply.github.com>
2023-03-27 14:37:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from train import trainerr
 import config
 from data_generation import coco_int
-from data_generation import traind
-from data_generation import vald
-from data_generation import testd
 from utility import visual
 import utility
 import inference
@@ -13,11 +10,11 @@ if __name__ == "__main__":
     #link for roboflow dataset
     roboflow_link = "https://app.roboflow.com/ds/KhjLZS7DRn?key=04wQwy2VHP"  
 
-    #traindm = "/content/train/_annotations.coco.json"
-    #valdm = "/content/valid/_annotations.coco.json"
-    #testdm = "/content/test/_annotations.coco.json"
+    traindm = "/content/train/_annotations.coco.json"
+    valdm = "/content/valid/_annotations.coco.json"
+    testdm = "/content/test/_annotations.coco.json"
 
-    coco_int(traind, vald, testd)
+    coco_int(traindm, valdm, testdm)
     print("Visualization of training data:")
 
     utility.visual()
